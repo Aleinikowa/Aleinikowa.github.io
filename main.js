bf252bb34dd53ac7b06c430b1e05cc0e647b59fc
Polina <polinaalejnikowa@gmail.com>
2021-09-01 10:38:35 +0300

    fix resume
    add new resume

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-let nav = document.getElementsByClassName('nav'),
-    text = document.getElementById('polina'),
+let text = document.getElementById('polina'),
     front = document.getElementsByClassName('front')[0],
     aboutHeader = document.getElementsByClassName('aboutHeader')[0],
     aboutText = document.getElementsByClassName('aboutText')[0],
@@ -8,10 +7,6 @@ let nav = document.getElementsByClassName('nav'),
     portfolioHeader = document.getElementsByClassName('portfolio-wrapper__header')[0],
     slider = document.getElementsByClassName('slider')[0],
     footer = document.getElementsByClassName('wrapper')[0],
-    btnNext = document.getElementsByClassName('btn-right')[0],
-    btnPrev = document.getElementsByClassName('btn-left')[0],
-    track = document.getElementsByClassName('slider-track')[0];
-    span = document.getElementsByClassName('slider-span'),
     btnMain = document.getElementById('btn-main'),
     about = document.getElementById('about'),
     aboutBlock = document.getElementsByClassName('about')[0],
@@ -20,8 +15,7 @@ let nav = document.getElementsByClassName('nav'),
     portfolio = document.getElementById('portfolio'),
     portfolioBlock = document.getElementsByClassName('portfolio')[0],
     contacts = document.getElementById('contacts'),
-    decorationBlock = document.getElementsByClassName('decoration')[0],
-    position = 0;
+    decorationBlock = document.getElementsByClassName('decoration')[0];
 
 btnMain.onclick = () => {
     let heightMain = mainBlock.clientHeight;
@@ -97,119 +91,27 @@ function animationSkills() {
     myLoop();  
 }
 
-window.onload = () => {
-    let width = document.documentElement.clientWidth,
-        position = 0;
+window.addEventListener('scroll', function() {
+    const heightScroll = window.pageYOffset;
+    console.log(window.pageYOffset)
 
-    checkBtns();
 
-    if (width > 860) {
-        showCarouselWithThreeItems();
-    }
-    if (width < 1170) {
-        showSmallCarousel();
+    if (heightScroll >= 400 && heightScroll <= 980) {
+        animation(aboutHeader);
+        setTimeout(animation(aboutText), 500);
     }
 
-    window.addEventListener('scroll', function() {
-        const heightScroll = window.pageYOffset;
-        console.log(window.pageYOffset)
-
-
-        if (heightScroll >= 400 && heightScroll <= 980) {
-            animation(aboutHeader);
-            setTimeout(animation(aboutText), 500);
-        }
-
-        if (heightScroll > 1000 && heightScroll < 1580) {
-            animation(skillsHeader);
-            setTimeout(animationSkills(), 100);
-        }
-
-        if (heightScroll > 1600 && heightScroll < 2400) {
-            animation(portfolioHeader);
-            setTimeout(animation(slider), 500);
-        }
-        if (heightScroll > 2380 && heightScroll < 4440) {
-            animation(portfolioHeader);
-            setTimeout(animation(footer), 100);
-        }
-    });
-    function checkBtns(windowWidth) {
-        btnPrev.disabled = position === 0;
-        btnNext.disabled = position <= -(windowWidth * 3);
-    };
-}
-
-window.addEventListener('resize', function(){
-    let width = document.documentElement.clientWidth;
-
-    if (width > 860) {
-        showCarouselWithThreeItems();
-    }
-    if (width < 1170) {
-        showSmallCarousel();
+    if (heightScroll > 1000 && heightScroll < 1580) {
+        animation(skillsHeader);
+        setTimeout(animationSkills(), 100);
     }
 
+    if (heightScroll > 1600 && heightScroll < 2400) {
+        animation(portfolioHeader);
+        setTimeout(animation(slider), 500);
+    }
+    if (heightScroll > 2380 && heightScroll < 4440) {
+        animation(portfolioHeader);
+        setTimeout(animation(footer), 100);
+    }
 });
-
-function showCarouselWithThreeItems() {
-    let item = document.getElementsByClassName('card'),
-        itemWidth = item[0].offsetWidth,
-        movePosition = itemWidth + 4;
-    
-    btnPrev.onclick = () => {
-        position += movePosition;
-        track.style.transform = `translateX(${position}px)`;
-
-        let spanData = document.getElementById('span1'),
-            counter = spanData.getAttribute('data-counter');
-            tempSpan = span[counter - 2];
-
-        tempSpan.classList.add('color');
-        span[counter - 1].classList.remove('color');
-        spanData.dataset.counter = counter - 1;
-
-        
-        btnPrev.disabled = position === 0;
-        btnNext.disabled = position <= -(movePosition * 3);
-    };
-    
-    btnNext.onclick = () => {
-        position -= movePosition;
-        track.style.transform = `translateX(${position}px)`;
-
-        let spanData = document.getElementById('span1'),
-            counter = spanData.getAttribute('data-counter'),
-            tempSpan = span[counter];
-
-        tempSpan.classList.add('color');
-        span[--counter].classList.remove('color');
-        spanData.dataset.counter =  counter + 2;
-
-        btnPrev.disabled = position === 0;
-        btnNext.disabled = position <= -(movePosition * 3);
-    };
-}
-
-function showSmallCarousel() {
-    let item = document.getElementsByClassName('slider'),
-    itemWidth = item[0].offsetWidth,
-    movePosition = itemWidth;
-
-    btnPrev.onclick = () => {
-        position += movePosition;
-        track.style.transform = `translateX(${position}px)`;
-        
-        btnPrev.disabled = position === 0;
-        btnNext.disabled = position <= -(movePosition * 5);
-    };
-
-    btnNext.onclick = () => {
-        position -= movePosition;
-        track.style.transform = `translateX(${position}px)`;
-
-        btnPrev.disabled = position === 0;
-        btnNext.disabled = position <= -(movePosition * 5);
-    };
-}
-
